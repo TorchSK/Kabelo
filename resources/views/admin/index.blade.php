dc091cc5f15caf871c21e5b9bffcd3d2b9c7b84b
TorchSK <gtorch@gmail.com>
2018-02-25 21:11:57 +0100

    aa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
     	@if ($categories->count() > 0 )
 		@foreach ($categories as $category)
     	<div class="item category" data-id={{$category->id}}>
-			<a class="name" href="/admin/category/{{$category->id}}/products">{{$category->name}}</a>
+			<a class="name" href="{{route('admin.category',['category'=>$category->url])}}">{{$category->name}}</a>
 			<div class="no_of_items">produktov: {{$category->products()->count()}}</div>
 			<a class="admin_delete_category_btn ui red label">Zmaž</a>
 			<a href="/category/{{$category->id}}/edit" class="admin_edit_category_btn ui brown label">Zmeň</a>
