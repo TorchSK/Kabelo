5950a8dfa625d364f96cc7dc8d565aa82e3564d4
TorchSK <gtorch@gmail.com>
2018-02-27 07:58:51 +0100

    hh

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -106,6 +106,7 @@ class ProductController extends Controller
         {
           $image = Image::make($destinationPath.'/'.$filename)
                ->widen($w)
+               ->resizeCanvas($w, $w)
                ->save($destinationPath.'/'.$filename);
 
           return url($destinationPath.'/'.$filename);
