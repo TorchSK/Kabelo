2f09a3ea63e7cc16b96c9662238c1e4802daf5b3
TorchSK <gtorch@gmail.com>
2018-07-03 12:34:14 +0200

    aa

--- app/PriceLevel.php
+++ app/PriceLevel.php
@@ -13,4 +13,23 @@ class PriceLevel extends Model {
  		return $this->belongsTo('App\Product');
  	}
 
+    public function setMocRegularAttribute($value)
+    {
+        $this->attributes['moc_regular'] =  floatval(str_replace(',', '.', $value));
+    }
+
+    public function setMocSaleAttribute($value)
+    {
+        $this->attributes['moc_sale'] =  floatval(str_replace(',', '.', $value));
+    }
+
+    public function setVocRegularAttribute($value)
+    {
+        $this->attributes['voc_regular'] =  floatval(str_replace(',', '.', $value));
+    }
+
+    public function setVocSaleAttribute($value)
+    {
+        $this->attributes['voc_sale'] =  floatval(str_replace(',', '.', $value));
+    }
 }
