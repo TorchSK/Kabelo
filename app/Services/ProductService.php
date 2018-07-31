0fe6b18d0ba82c4eaa73e6fd7eac80a87226005c
TorchSK <gtorch@gmail.com>
2018-07-31 21:04:09 +0200

    aa

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -132,7 +132,7 @@ class ProductService implements ProductServiceContract {
             $sortBy = $this->getUserPriceType();
         }
         
-        $products = $this->query($filters)->orderBy($sortBy,$sortOrder)->paginate(24);
+        $products = $this->query($filters)->orderBy($sortBy,$sortOrder)->get();
 
         // set price range
         $priceRangeFilters = $filters;
@@ -187,7 +187,7 @@ class ProductService implements ProductServiceContract {
         $activeFilters = collect($filters);
 
         // set products
-        $products = $this->query($filters)->orderBy($sortBy,$sortOrder)->get();
+        $products = $this->query($filters)->orderBy($sortBy,$sortOrder)->paginate(24);
         // set price range
         $priceRangeFilters = $filters;
         unset($priceRangeFilters['price']);
