8b489fa6bada00ff0e87a0058b68af1752289a0d
TorchSK <gtorch@gmail.com>
2018-08-02 20:59:37 +0200

    aa

--- resources/views/products/row.blade.php
+++ resources/views/products/row.blade.php
@@ -77,8 +77,8 @@
 	</div>
 	@elseif(Request::segment(1) == 'admin')
 		<div class="actions ui fluid icon buttons">
-		<a href="/product/create?duplicate={{$product->id}}" class="ui teal small button"><i class="copy icon"></i></a>
 		<a href="/{{$product->maker}}/{{$product->code}}/edit" class="ui blue small button"><i class="edit icon"></i></a>
+		<a href="/product/create?duplicate={{$product->id}}" class="ui teal small button"><i class="copy icon"></i></a>
 		<a class="ui red small button product_row_delete_btn"><i class="delete icon"></i></a>
 		</div>	
 	@endif
