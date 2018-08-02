bbaa0c43e008eb3e0f5f93a4cb54dd23cdb4c0b9
TorchSK <gtorch@gmail.com>
2018-08-02 14:34:42 +0200

    aa

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -174,7 +174,6 @@ class ProductService implements ProductServiceContract {
         }
 
         $category = Category::find($request->get('category'));
-
         $sortBy = $request->get('sortBy');
         $sortOrder = $request->get('sortOrder');    
 
@@ -230,9 +229,8 @@ class ProductService implements ProductServiceContract {
             $makers = $makers->unique(['maker']);
 
             $categoryParameters = $category->parameters;
-            $categoryChildren = $category->children;
 
-            foreach($categoryChildren as $child)
+            foreach($category->children as $child)
             {
                 $categoryParameters = $categoryParameters->merge($child->parameters);
                 $makers = $makers->union($child->products->unique(['maker']));
@@ -295,9 +293,25 @@ class ProductService implements ProductServiceContract {
         $categoryCounts = [];
         $categoryCounts['categories'] = [];
 
-        foreach (Category::with('products')->get() as $category)
+        foreach (Category::with(['children','products','children.products','children.children.products'])->get() as $category)
         {
             $categoryCounts['categories'][$category->id] = $category->products->where('active',1)->count();
+
+            if ($category->children->count() > 0)
+            {
+                foreach ($category->children as $child)
+                {
+                    $categoryCounts['categories'][$category->id] += $child->products->where('active',1)->count();
+
+                    if ($child->children->count() > 0)
+                    {
+                        foreach ($child->children as $subchild)
+                        {
+                            $categoryCounts['categories'][$category->id] += $subchild->products->where('active',1)->count();
+                        }
+                    }
+                }
+            }
         }
 
         return $categoryCounts;
