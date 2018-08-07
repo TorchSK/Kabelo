3fdacb4ff5fe29627a8425228474c3b01f6c4cee
TorchSK <gtorch@gmail.com>
2018-08-07 10:11:07 +0200

    aa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
   <input type="submit" class="ui brown big fluid button" value="Login">
 
   <div class="ui horizontal divider">Alebo</div>  
-  <div class="inline field"><a href="#">Reset hesla</a></div>
+  <div class="inline field"><a href="/password/reset" id="reset_pwd_link"><i class="lock icon"></i> Zabudnuté heslo</a></div>
 
 </form>
 
