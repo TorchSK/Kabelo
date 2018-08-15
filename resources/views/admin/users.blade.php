c16ef0d9c6d1f3388733074157a84135db56f139
TorchSK <gtorch@gmail.com>
2018-08-15 23:39:45 +0200

    aa

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -7,6 +7,7 @@
 	<table class="ui celled selectable sortable table">
 	  <thead>
 	    <tr>
+	    <th></th>
 	    <th>ID</th>
 	 	<th>Meno</th>
 	    <th>Email</th>
@@ -14,12 +15,15 @@
 	   	<th>VOC</th>
 	   	<th>Datum registrácie</th>
 	   	<th>Počet objednávok</th>
-	   	<th></th>
 
 	  </tr></thead>
 	  <tbody>
 	  	@foreach(App\User::orderBy('created_at','desc')->get() as $user)
 		<tr>
+		  <td class="collapsing">
+	      	<a href="{{route('admin.userDetail',['user'=>$user->id])}}" class="ui mini icon blue button"><i class="search large icon"></i></a>
+	      </td>
+
 	      <td>{{$user->id}}</td>
 	   	  <td>{{$user->name}}</td>
 	      <td>{{$user->email}}</td>
@@ -33,9 +37,6 @@
 	      <td>{{Carbon\Carbon::parse($user->created_at)->format('d.m.Y H:i:s')}}</td>
 	      <td>{{$user->orders->count()}}</td>
 
-	      <td class="collapsing">
-	      	<a href="{{route('admin.userDetail',['user'=>$user->id])}}" class="ui mini icon blue button"><i class="search large icon"></i></a>
-	      </td>
 
 
 	  	</tr>
