04e2a91cad754b8154fca2e87621bcf4d1cde5c8
TorchSK <gtorch@gmail.com>
2018-08-18 12:23:41 +0200

    aa

--- resources/views/auth/registersuccess.blade.php
+++ resources/views/auth/registersuccess.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="ct flex_content content  ct">
+<div class="ct flex_content content order_success ct">
 	<div>
 		<i class="huge checkmark icon"></i>
 		<div class="msg">Pokračujte vo svojej emailovej schránke aktiváciou účtu</div>
