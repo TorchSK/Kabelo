c7e4e27ad245591f3db3f385ee2d36e09f1f5b8d
TorchSK <gtorch@gmail.com>
2018-08-18 12:22:58 +0200

    aa

--- resources/views/auth/registersuccess.blade.php
+++ resources/views/auth/registersuccess.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="pad wrapper ct">
-<div class="order_success ct">
+<div class="ct flex_content content  ct">
 	<div>
 		<i class="huge checkmark icon"></i>
 		<div class="msg">Pokračujte vo svojej emailovej schránke aktiváciou účtu</div>
 	</div>
-</div>
+
 </div>
 @stop
--- resources/views/orders/success.blade.php
+++ resources/views/orders/success.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="ct flex_content order_success">
+<div class="ct flex_content content order_success">
 	<div>
 		<i class="huge checkmark icon"></i>
 		<div class="msg">Objednávka úspešne odoslaná</div>
