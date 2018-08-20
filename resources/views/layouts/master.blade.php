22959cf8432fc2c885fab8f043ba9bcee1b7260a
TorchSK <gtorch@gmail.com>
2018-08-20 15:00:05 +0200

    aa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
     @include('includes.head')
 
     <body @if (isset($bodyid)) id="{{$bodyid}}" @endif data-layout="{{App\Setting::whereName('layout')->first()->value}}">
