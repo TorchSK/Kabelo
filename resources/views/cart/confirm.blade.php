08ed15bfa47ebf37c7bc23583ddfef3107cf1327
TorchSK <gtorch@gmail.com>
2018-08-22 13:05:21 +0200

    aa

--- resources/views/cart/confirm.blade.php
+++ resources/views/cart/confirm.blade.php
@@ -151,9 +151,11 @@
 	</div>
 
 	<div class="ct">
+		<div>
 		<div class="ui checkbox" id="agreements_checkbox">
 		  <input type="checkbox" name="example">
 		  <label>Suhlasím s <a href="/obchodne-podmienky" target="_blank">obchodnými podmienkami</a> a <a href="/gdpr" target="_blank">ochranou osobných údajov</a></label>
+		</div>
 		</div>
 		<a class="ui huge disabled green button" id="submit_order_btn"><i class="upload icon"></i>Odoslať objednávku</a>
 	</div>
