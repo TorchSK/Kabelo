a7aa8477df159274773b58df99b7319f598f7a9d
TorchSK <gtorch@gmail.com>
2018-08-23 14:05:41 +0200

    aa

--- resources/views/products/dedra/profile.blade.php
+++ resources/views/products/dedra/profile.blade.php
@@ -44,7 +44,7 @@
            @if ($product->images->count() == 0)
            <img src="/img/empty.jpg" class="ui image" />
            @elseif ($product->image)
-           <img src="{{$product->image->path}}" class="ui image" />
+           <img src="{{$product->image->path}}" class="ui image" style="max-height: 700px"/>
            @endif
         </div>
         
