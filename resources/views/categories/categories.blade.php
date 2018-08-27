d55c4ff40e32ddf7378795271c28d2aaa7b556a7
TorchSK <gtorch@gmail.com>
2018-08-27 21:49:55 +0200

    aa

--- resources/views/categories/categories.blade.php
+++ resources/views/categories/categories.blade.php
@@ -1,5 +1,5 @@
 <ul class="categories">
-    @foreach(App\Category::with('descendants')->get()->toTree() as $category)
+    @foreach(App\Category::with('descendants')->orderBy('order')->get()->toTree() as $category)
         @include('categories.row', ['requestCategory'=> App\Category::find(Request::get('category'))])
     @endforeach
 </ul>
