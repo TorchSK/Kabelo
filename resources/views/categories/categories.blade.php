43fdaf269a62f2e6b3778ad9d57127c7c09420ec
TorchSK <gtorch@gmail.com>
2018-08-27 19:26:21 +0200

    aa

--- resources/views/categories/categories.blade.php
+++ resources/views/categories/categories.blade.php
@@ -1,5 +1,5 @@
 <ul class="categories">
-    @foreach(App\Category::with('children','descendants')->whereNull('parent_id')->orderBy('order')->get() as $category)
+    @foreach(App\Category::whereNull('parent_id')->orderBy('order')->get() as $category)
         @include('categories.row', ['requestCategory'=> App\Category::find(Request::get('category'))])
     @endforeach
 </ul>
