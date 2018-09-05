73c5edbc39b7da82125324848c0e6e66c57ac372
TorchSK <gtorch@gmail.com>
2018-09-05 18:31:53 +0200

    ss

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -82,7 +82,7 @@ class AdminController extends Controller
 
     $len = count($input);
 
-    $firsthalf = array_slice($input, $len / 6, $len / 5);
+    $firsthalf = array_slice($input, $len / 5, $len / 4);
     $secondhalf = array_slice($input, $len / 2);
 
 
