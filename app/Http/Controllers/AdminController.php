7caca13374600b86af462df22deb3fa134f20bcc
TorchSK <gtorch@gmail.com>
2018-09-05 18:30:30 +0200

    aa

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -82,7 +82,7 @@ class AdminController extends Controller
 
     $len = count($input);
 
-    $firsthalf = array_slice($input, $len / 7, $len / 6);
+    $firsthalf = array_slice($input, $len / 6, $len / 5);
     $secondhalf = array_slice($input, $len / 2);
 
 
