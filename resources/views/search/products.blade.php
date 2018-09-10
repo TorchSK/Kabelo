e8e935d3b40b772557913cb061e19015e1cd054b
TorchSK <gtorch@gmail.com>
2018-09-10 16:09:23 +0200

    aa

--- resources/views/search/products.blade.php
+++ resources/views/search/products.blade.php
@@ -4,7 +4,7 @@
 			@if ($product->images->count() == 0)
 			<img src="/img/empty.jpg" class="ui image" />
 				@elseif ($product->image)
-		           <img src="{{$product->image->path}}" class="ui image" />
+		           <img src="{{url($product->image->path)}}" class="ui image" />
 			@endif
 		</div>
 		<div class="name">{{$product->name}}</div>
