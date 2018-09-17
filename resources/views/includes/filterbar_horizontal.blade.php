8812ea2ab3f98d608b99761ee39459274aed489e
TorchSK <gtorch@gmail.com>
2018-09-17 13:10:26 +0200

    aa

--- resources/views/includes/filterbar_horizontal.blade.php
+++ resources/views/includes/filterbar_horizontal.blade.php
@@ -6,9 +6,18 @@
 
     	<div class="ui fluid basic popup bottom left transition hidden" id="cat_popup_{{$category->id}}">
 
+    		<div class="subcats">
 			@foreach($category->children as $child)
-				<div>{{$child->name}}</div>
-			@endforeach
+				<div class="sub1">
+					<a href="{{route('home.category',['category'=>$child->url])}}" class="name">{{$child->name}}</a>
+							@foreach($child->children as $child2)
+								<div class="sub2">
+									<a href="{{route('home.category',['category'=>$child2->url])}}" class="name">{{$child2->name}}</a>
+								</div>
+							@endforeach	
+				</div>
+			@endforeach	
+			</div>
 
 		</div>
 
