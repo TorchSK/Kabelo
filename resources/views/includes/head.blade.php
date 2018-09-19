269e1f6c1506ddb3ca1a834f7528d73548656c0f
TorchSK <gtorch@gmail.com>
2018-09-19 20:47:54 +0200

    aa

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="google-site-verification" content="CY9IwgJZGjaR_icVxrolcCKFZUjWhHfx9QDnJ32_MLE" />
 
-    <title>{{$appname}}.sk</title>
+    <title>{{App\Setting::firstOrCreate(['name'=>'company_name'])->value}}.sk</title>
 
     <link rel="icon" type="image/png" href="{{url('img/'.$appname)}}_favico.png" />
 
