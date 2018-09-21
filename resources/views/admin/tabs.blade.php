da10348bd2a5b8647a1db6bff155cf7916dce835
TorchSK <gtorch@gmail.com>
2018-09-21 08:48:48 +0200

    aa

--- resources/views/admin/tabs.blade.php
+++ resources/views/admin/tabs.blade.php
@@ -1,20 +1,28 @@
-<div id="admin_menu" class="ui vertical menu">
+<div id="admin_menu" class="ui vertical accordion menu">
 
   <div class="ct">
   <img src="{{url('img/adminLogo.png')}}" style="padding: 15px; display: inline-block;" width="140" />
   </div>
 
   <div class="item">
-    <div class="header"><i class="chart pie icon"></i> Prehľad</div>
-    <div class="menu">
+    <div class="header title">
+      <i class="chart pie icon"></i>
+      Prehľad
+      <i class="dropdown icon"></i>
+    </div>
+    <div class="content menu">
       <a href="{{route('admin.dashboard.new')}}" class="item">Najnovšie</a>
       <a href="{{route('admin.dashboard.overall')}}" class="item">Súhrnné</a>
     </div>
   </div>
 
   <div class="item">
-	<div class="header"><i class="cube icon"></i> Eshop</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="cube icon"></i>
+      Eshop
+      <i class="dropdown icon"></i>
+    </div>
+    <div class="content menu">
       <a href="{{route('admin.eshop.categories')}}" class="item">Zoznam kategórií</a>
       <a href="{{route('admin.eshop.products',['category'=>App\Category::first()->url])}}" class="item">Kategórie / Produkty</a>
       <a href="{{route('admin.eshop.new')}}" class="item">Novinky</a>
@@ -24,29 +32,45 @@
   </div>
 
   <div class="item">
-	<div class="header"><i class="tasks icon"></i> Parametre</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="tasks icon"></i>
+      <i class="dropdown icon"></i>
+      Parametre
+    </div>
+    <div class="content menu">
       <a href="{{route('admin.params.index')}}" class="item">Zoznam parametrov</a>
     </div>
   </div>
 
   <div class="item">
-	<div class="header"><i class="users icon"></i> Uživatelia</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="users icon"></i>
+      <i class="dropdown icon"></i>
+      Uživatelia
+    </div>
+    <div class="menu content">
       <a href="{{route('admin.users.index')}}" class="item">Zoznam uživateľov</a>
     </div>
   </div>
 
   <div class="item">
-	<div class="header"><i class="cart icon"></i> Objednávky</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="cart icon"></i>
+      <i class="dropdown icon"></i>
+      Objednávky
+    </div>
+    <div class="menu content">
       <a href="{{route('admin.orders.index')}}" class="item">Objednávky</a>
     </div>
   </div>
 
   <div class="item">
-	<div class="header"><i class="settings icon"></i> Nastavenia</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="settings icon"></i>
+      <i class="dropdown icon"></i>
+      Nastavenia
+    </div>
+    <div class="menu content">
       <a href="{{route('admin.settings.banners')}}" class="item">Bannery</a>
       <a href="{{route('admin.settings.eshop')}}" class="item">Eshop</a>
       <a href="{{route('admin.settings.delivery')}}" class="item">Doprava</a>
@@ -55,14 +79,23 @@
 
   </div>
     <div class="item">
-	<div class="header"><i class="file icon"></i> Súbory</div>
-    <div class="menu">
+	<div class="header title">
+    <i class="file icon"></i>
+      <i class="dropdown icon"></i>
+      Súbory
+    </div>
+    <div class="menu content">
       <a href="{{route('admin.files')}}" class="item">Zoznam súborov</a>
     </div>
   </div>
-    <div class="item">
-	<div class="header"><i class="tv icon"></i> Šáblóna webu</div>
-    <div class="menu">
+
+  <div class="item">
+	<div class="header title">
+    <i class="tv icon"></i>
+      <i class="dropdown icon"></i>
+      Šáblóna webu
+    </div>
+    <div class="content menu">
       <a href="{{route('admin.layout')}}" class="item">Zoznam šablón</a>
     </div>
   </div>
