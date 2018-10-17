5eb18b76f8f49f00b2b35b08cc57684650101c39
TorchSK <gtorch@gmail.com>
2018-10-17 21:02:59 +0200

    aa

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'kabelo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,12 +39,12 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'copper' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE_COPPER', 'copper'),
+            'username' => env('DB_USERNAME', 'copper'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
