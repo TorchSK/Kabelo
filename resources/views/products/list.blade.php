03ef4ab041ae17e4f8cb0ef50caa19efad6ab7c2
TorchSK <gtorch@gmail.com>
2019-01-08 14:28:55 +0100

    aa

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -2,6 +2,6 @@
 	@include('products.row',['grid'=>1])
 @endforeach
 
-<div class="scroll_to_top"><i class="big icon angle double up"></i></div>
+<div class="scroll_to_top"><i class="big icon teal circular inverted angle double up"></i></div>
 
 <a href="{{ $products->nextPageUrl()}}" class="hidden" id="next_page" data-next="{{$products->hasMorePages()}}"></a>
