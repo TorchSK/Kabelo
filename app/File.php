70a82b891fddfb1e7a7cde3667aeca597934fdcc
TorchSK <gtorch@gmail.com>
2019-01-10 17:03:40 +0100

    aa

--- app/File.php
+++ app/File.php
@@ -27,8 +27,16 @@ class File extends Model {
 
       if(isset($array[5]))
       {
-    	$path = 'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
-        return $path;
+        $path = 'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
+        
+        if (file_exists($path))
+        {
+          return $path;
+        }
+        else
+        {
+          'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o/'.explode('.',$array[7])[0].'.jpeg';
+        }
       }
       else{
         return $this->path;
