7a866e959e30a29b8eac60e541fc66a21469dc29
TorchSK <gtorch@gmail.com>
2019-01-18 21:40:47 +0100

    aa

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -124,7 +124,7 @@
           </div>
 
           <div id="cart_popup" class="ui basic popup transition"> 
-              @if (count($cart['items']) > 0)
+              @if (isset($cart) && count($cart['items']) > 0)
                 @if (Auth::check())
                   @foreach($cart->products as $product)
                     @include('cart.row')
