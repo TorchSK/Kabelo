f8cac55912adb975bc2bfa3ba222b555570781c6
TorchSK <gtorch@gmail.com>
2019-01-22 19:49:56 +0100

    aaaa

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -17,17 +17,17 @@
         </style>
     </head>
     
-    <body style="width: 100%; margin: 0; padding: 0; font-family: 'raleway-regular',Helvetica,Arial,sans-serif; font-size: 14px; font-weight: 300;">
+    <body style="width: 100%; margin: 0; padding: 0; font-family: 'Roboto',sans-serif; font-size: 15px; font-weight: 300;">
     
     <div style="border-top: 15px #0d345b solid; border-bottom: 1px solid #EEE; padding: 30px; color: #FFF; text-align: center; box-sizing: content-box">
-        <img src="<?php echo $message->embed(public_path().'/img/email_logo_'.$appname.'.png'); ?>" width="191">
+        <img src="<?php echo $message->embed(public_path().'/img/email_logo_'.$appname.'.png'); ?>" width="60">
     </div>
 
     <div style="padding: 50px; text-align: center;border-radius: 0 0 6px 6px;">
 
-        <div style="margin: 15px 0;">Ďakujeme za registráciu na eshope Dedraslovakia.sk</div>
+        <div style="margin: 15px 0; font-family: 'Roboto',sans-serif; font-size: 15px; font-weight: 300;">Ďakujeme za registráciu na eshope Dedraslovakia.sk</div>
 
-        <div style="margin: 15px 0; margin-bottom: 35px;">Svoju registráciu dokončite prosím klikom na tlačítko nižšie</div>
+        <div style="margin: 15px 0; margin-bottom: 35px; font-family: 'Roboto',sans-serif; font-size: 15px; font-weight: 300;">Svoju registráciu dokončite prosím klikom na tlačítko nižšie</div>
         <a href="{!! url("/user/activate").'/'.$token !!}" style="padding: 10px 30px; background-color: #f22b6c; margin-top: 35px; text-decoration: none; color: white; font-weight: 900; border-radius: 5px;">Aktivovať učet</a>
     </div>
 
