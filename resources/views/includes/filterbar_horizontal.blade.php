06bbb2e746618c6fa5b92624d608f31dc76e217b
TorchSK <gtorch@gmail.com>
2019-01-26 22:30:12 +0100

    aaa

--- resources/views/includes/filterbar_horizontal.blade.php
+++ resources/views/includes/filterbar_horizontal.blade.php
@@ -2,7 +2,7 @@
 	<div class="container" data-item="filterbar_container" style="background-color: {{App\Setting::firstOrCreate(['name'=>'filterbar_container_background-color'])->value}};">
     @foreach(App\Category::where('active',1)->orderBy('order')->get()->toTree()->take(5) as $category)
 
-    	<a href="{{route('slug',['category'=>$category->full_url])}}" class="item @if(isset($requestCategory) && ($category->id == $requestCategory->id || in_array($category->id,$requestCategory->ancestors->pluck('id')->toArray()))) active @endif" data-cat="{{$category->id}}">
+    	<a href="{{route('slug',['category'=>$category->full_url])}}" class="item top @if(isset($requestCategory) && ($category->id == $requestCategory->id || in_array($category->id,$requestCategory->ancestors->pluck('id')->toArray()))) active @endif" data-cat="{{$category->id}}">
     		@if(App\Setting::firstOrCreate(['name'=>'filterbar_image'])->value == 1)
     		<div class="image_div">
     			<img src="{{url($category->image)}}" alt="{{$category->name}}"/>
