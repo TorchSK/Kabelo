94ffb8580db9d17596dd5e4519127d9446dcfb6e
TorchSK <gtorch@gmail.com>
2019-02-01 13:57:55 +0100

    aa

--- app/Mail/NewOrder.php
+++ app/Mail/NewOrder.php
@@ -42,7 +42,7 @@ class NewOrder extends Mailable
         $this->delivery_method = Order::where('id', $this->order['id'])->first()->delivery;
         $this->payment_method = Order::where('id', $this->order['id'])->first()->payment;
 
-        return $this->from('kabelo@kabelo.sk')
+        return $this->from('obchod@dedraslovakia.sk')
                      ->subject('Objednávka č. '.$this->order['id'])
                      ->view('emails.neworder')
                      ->attach(storage_path('app/Obchodne podmienky.pdf'))
