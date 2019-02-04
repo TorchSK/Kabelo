47adee368634c6753623ff4885998e751c024999
TorchSK <gtorch@gmail.com>
2019-02-04 12:29:25 +0100

    aa

--- resources/views/includes/filterbar_horizontal.blade.php
+++ resources/views/includes/filterbar_horizontal.blade.php
@@ -26,7 +26,7 @@
 			@endforeach	
 
 			<div class="sub1">
-					<img src="/img/menu_{{$category->url}}.png" />
+					<img src="/img/menu_{{$category->url}}.png" alt="{{$cat->name}}" />
 			</div>
 
 			</div>
