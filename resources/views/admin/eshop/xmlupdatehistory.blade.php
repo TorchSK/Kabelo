4236cb3f1052e4042fc185524b013ea33d596cbb
TorchSK <gtorch@gmail.com>
2019-02-05 11:11:24 +0100

    aa

--- resources/views/admin/eshop/xmlupdatehistory.blade.php
+++ resources/views/admin/eshop/xmlupdatehistory.blade.php
@@ -3,7 +3,7 @@
 
     <div id="admin_xmlupdate_wrapper" class="admin_wrapper">
 		
-		<div class="ui accordion">
+		<div class="ui styled accordion">
 			@foreach(App\Log::groupBy('created_at')->get(['created_at']) as $log)
 		  		<div class="title">
 				    <i class="dropdown icon"></i>
@@ -16,7 +16,21 @@
 
 		  		<div class="content">
 
-			  @foreach(App\Log::whereType($log->type)->whereCreatedAt($log->created_at)->get() as $content)
+			  	@foreach(App\Log::whereType($log->type)->whereCreatedAt($log->created_at)->get() as $content)
+			  		<div class="accordion">
+			  			<div class="title">
+						    <i class="dropdown icon"></i>
+						    @if($content->operation=='new_categories')
+						    Pridané kategórie ({{collect($content->log)->count()}})
+						    @endif
+						    @if($content->operation=='new_products')
+						    Pridané produkty ({{collect($content->log)->count()}})
+						    @endif						   
+						    @if($content->operation=='removed_products')
+						    Zneaktivnené produkty ({{collect($content->log)->count()}})
+						    @endif
+				  		</div>
+
 				    <p class="transition hidden">
 				    	@if(is_array($content->log) && count($content->log) > 0)
 					    	@foreach($content->log as $item)
@@ -24,7 +38,8 @@
 					    	@endforeach
 				    	@endif
 				    </p>
-			  @endforeach
+					</div>
+			 	@endforeach
 
 			</div>
 
