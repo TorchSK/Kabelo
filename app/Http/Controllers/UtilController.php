d22c779180c0ff6af3fd5ae7bdf2e03a92e54b0d
TorchSK <gtorch@gmail.com>
2019-02-05 19:10:43 +0100

    aa

--- app/Http/Controllers/UtilController.php
+++ app/Http/Controllers/UtilController.php
@@ -139,7 +139,12 @@ class UtilController extends Controller
 
     public function searchAll($query)
     {
-        $data['products'] = Product::where('name', 'like', '%'.$query.'%')->orWhere("desc", "like", "%".$query."%")->orWhere("code", "like", "%".$query."%")->whereActive(1)->take(5)->get();
+        $data['products'] = Product::where(function ($query) {
+                $query->where('name', 'like', '%'.$query.'%')
+                      ->orWhere("desc", "like", "%".$query."%")
+                      ->orWhere("code", "like", "%".$query."%")
+                })->whereActive(1)->take(5)->get();
+        
         $data['users'] = User::where('name', 'like', '%'.$query.'%')->orWhere("email", "like", "%".$query."%")->take(5)->get();
 
         return response()->json(['products'=>view('search.products', $data)->render(), 'users'=>view('search.users', $data)->render()]);
