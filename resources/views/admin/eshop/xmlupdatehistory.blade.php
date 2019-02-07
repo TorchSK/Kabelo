043b587282321a3ff7fbb22653dfffd8bdc74f51
TorchSK <gtorch@gmail.com>
2019-02-07 09:48:42 +0100

    aa

--- resources/views/admin/eshop/xmlupdatehistory.blade.php
+++ resources/views/admin/eshop/xmlupdatehistory.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin')
 @section('content')
 
-    <div id="admin_xmlupdate_wrapper" class="admin_wrapper">
+<div id="admin_xmlupdate_wrapper" class="admin_wrapper">
 		
-		<div class="ui styled accordion">
+		<div class="ui fluid styled accordion">
 			@foreach(App\Log::groupBy('created_at')->get(['created_at']) as $log)
 		  		<div class="title">
 				    <i class="dropdown icon"></i>
@@ -31,21 +31,32 @@
 						    @endif
 				  		</div>
 
-				    <p class="transition hidden">
-				    	@if(is_array($content->log) && count($content->log) > 0)
-					    	@foreach($content->log as $item)
-					    		{{$item['id']}}
-					    	@endforeach
-				    	@endif
-				    </p>
-					</div>
+				  		<div class="content">
+						    	@if(is_array($content->log) && count($content->log) > 0)
+							    	@foreach($content->log as $item)
+							    		<div class="item">
+							    			<div class="id">{{$item['id']}}</div>
+							    			@if(isset($item['code']))
+							    			<div class="image"><img src="{{App\File::find($item['id'])->path}}" /></div>
+							    			@endif
+							    			@if(isset($item['code']))
+							    			<div class="code">{{$item['code']}}</div>
+							    			@endif
+							    			<div class="name">{{$item['name']}}</div>
+
+							    		</div>
+							    	@endforeach
+						    	@endif
+						</div>
 			 	@endforeach
+			 </div>
 
 			</div>
 
 		  @endforeach
 		</div>
+</div>
 
-    </div>
+
 
 @stop
