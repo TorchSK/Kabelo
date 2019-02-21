2c850bf9ccba188f4a165e0404a6c20f61ccce42
TorchSK <gtorch@gmail.com>
2019-02-21 21:52:00 +0100

    ss

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -57,7 +57,7 @@ class ProductService implements ProductServiceContract {
                 $query->whereHas('stickers');
             }
 
-            if ($request->has('inactive_only') && $request->get('inactive_only')=='true')
+            if ($request->has('inactive_only') && $request->get('inactive_only'))
             {
                 $query->whereActive(0);
             }
