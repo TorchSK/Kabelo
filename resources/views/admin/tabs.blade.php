901ad21971dbe06bebd6013eb5b390f49a46eee9
TorchSK <gtorch@gmail.com>
2019-02-21 15:22:00 +0100

    aa

--- resources/views/admin/tabs.blade.php
+++ resources/views/admin/tabs.blade.php
@@ -28,7 +28,6 @@
       <a href="{{route('admin.eshop.new')}}" class="item @if(Route::currentRouteName()=='admin.eshop.new') active @endif">Novinky ({{App\Product::whereNew(1)->count()}})</a>
       <a href="{{route('admin.eshop.sale')}}" class="item @if(Route::currentRouteName()=='admin.eshop.sale') active @endif">Zľavy ({{App\Product::whereSale(1)->count()}})</a>
       <a href="{{route('admin.eshop.bestsellers')}}" class="item @if(Route::currentRouteName()=='admin.eshop.bestsellers') active @endif">Bestsellery ({{App\Category::whereBestseller(1)->count()}} / {{App\Product::whereBestseller(1)->count()}})</a>
-      <a href="{{route('admin.eshop.inactive')}}" class="item @if(Route::currentRouteName()=='admin.eshop.inactive') active @endif">Neaktívne ({{App\Product::whereActive(0)->count()}})</a>
       <a href="{{route('admin.eshop.stickers')}}" class="item @if(Route::currentRouteName()=='admin.eshop.stickers') active @endif">Stickers</a>
       @if($appname=='dedra')
       <a href="{{route('admin.eshop.xmlupdate')}}" class="item @if(Route::currentRouteName()=='admin.eshop.xmlupdate') active @endif">Aktualizácia z XML</a>
