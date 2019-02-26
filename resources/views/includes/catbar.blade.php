d7c730832f1ce03819defc878dfa2f28d6db2d4e
TorchSK <gtorch@gmail.com>
2019-02-26 10:43:53 +0100

    aa

--- resources/views/includes/catbar.blade.php
+++ resources/views/includes/catbar.blade.php
@@ -2,13 +2,11 @@
 	@foreach (App\Category::whereNull('parent_id')->orderBy('order')->get() as $category)
     <a class="item" href="{{route('slug',['category'=>$category->full_url])}}">
      <text>{{$category->name}}</text>
-     <count>{{$categoryCounts['categories'][$category->id]}}</count>
     </a>
 
     		@foreach ($category->children->sortBy('order') as $child)
     		    <a class="item sub" href="{{route('slug',['category'=>$child->full_url])}}">
     			 <text>{{$child->name}}</text>
-    			 <count>{{$categoryCounts['categories'][$child->id]}}</count>
     			</a>
 
     		@endforeach
