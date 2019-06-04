e1008f1f5a153d6c5eec89eea4587f85177f2fe7
TorchSK <gtorch@gmail.com>
2019-06-04 18:07:49 +0200

    aa

--- resources/views/includes/dedra/footer.blade.php
+++ resources/views/includes/dedra/footer.blade.php
@@ -51,3 +51,6 @@
 
 </footer>	
 
+
+    <script src="/js/bootstrap.js"></script>
+    <script src="/js/app.js"></script>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -106,8 +106,6 @@
     </script>
     @endif
 
-    <script src="/js/bootstrap.js"></script>
-    <script src="/js/app.js"></script>
 
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
