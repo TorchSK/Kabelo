3e4e8ef55eb1bbb3192eb80861e6bd63b72a0dba
TorchSK <gtorch@gmail.com>
2019-12-09 13:40:29 +0100

    s

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -24,7 +24,7 @@
 
             <div class="grow">
                 <br />
-                <div class="ui olive message">
+                <div class="ui orange message">
                   <i class="close icon"></i>
                   
                   <p>Z dôvodu vianočných a novoročných sviatkov budeme expedovať tovar nasledovne:<br />
