3731f38ecd71c2ab9960549da6ecc9bd8a7c326c
TorchSK <gtorch@gmail.com>
2020-07-08 21:26:10 +0200

    aa

--- app/File.php
+++ app/File.php
@@ -24,16 +24,13 @@ class File extends Model {
     {
 		  $array = explode('/', $this->path);
 
-      $test = "/img/empty.jpg";
 
       if(isset($array[5]) && isset($array[7]))
       {
-    	 $path = 'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
-        return $test;
+    	$path = 'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
+        return $path;
       }
       else{
-                return $test;
-
         return $this->path;
       }
 
