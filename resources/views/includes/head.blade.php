dd2d70857fdf0dea74f3109b5c82a8a8be4b192f
TorchSK <gtorch@gmail.com>
2020-07-13 23:28:00 +0200

    aa

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -49,18 +49,7 @@
     <link rel="canonical" href="https://dedraslovakia.sk/@if(Request::path()!="/"){{Request::path()}}@endif">
 
 
-
-
-    <link media="all" type="text/css" rel="stylesheet" href="/css/reset.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/semantic.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/typekit.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/flickity.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/dropzone.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/cropper.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/modulobox.min.css">
-    <link media="all" type="text/css" rel="stylesheet" href="/css/handsontable.full.min.css">
-
-    <link media="all" type="text/css" rel="stylesheet" href="/css/{{$appname}}.css">
+    <link media="all" type="text/css" rel="stylesheet" href="/css/styles.pure.css">
 
 
 
