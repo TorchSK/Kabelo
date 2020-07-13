019413d410731b82a2a2f16987c7f58f55cc838b
TorchSK <gtorch@gmail.com>
2020-07-13 23:28:08 +0200

    aa

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,6 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.less('resources/assets/less/dedra/master.less','public/css/dedra.css').purgeCss();
+mix.less('resources/assets/less/dedra/master.less','public/css/dedra.css');
 mix.babel(['resources/assets/js/app.js','resources/assets/js/ui.js','resources/assets/js/semantic.js'],'public/js/app.js');
 mix.copy('resources/assets/js/admin.js','public/js/admin.js');
