07617558fbf2e21576985c9ee70d373032755969
TorchSK <gtorch@gmail.com>
2020-07-31 13:09:12 +0200

    aa

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,5 +1,5 @@
 @if(Request::segment(1)=='banner' || Request::segment(1)=='admin')
-<div class="banner lazy" loading="lazy" data-type="{{$type}}" data-id="{{$cover->id}}" data-src="{{url($cover->image)}}" style="@if(isset($width)) width: {{$width}}; @endif";>
+<div class="banner" data-type="{{$type}}" data-id="{{$cover->id}}" style="background-image: url({{url($cover->image)}}); @if(isset($width)) width: {{$width}}; @endif";>
 
     <div class="banner_options">
     	<a href="{{route('banner.edit',['cover'=>$cover->id, 'type'=> $type])}}" class="ui blue button">Zmeň</a>
