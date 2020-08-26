a144dfc5debecadc8c590fb5eeba7b802da1bbb0
TorchSK <gtorch@gmail.com>
2020-08-26 13:19:01 +0200

    aa

--- app/Http/Middleware/CreateCartCookie.php
+++ app/Http/Middleware/CreateCartCookie.php
@@ -18,6 +18,7 @@ class CreateCartCookie
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        /*
         if (!Auth::guard($guard)->check()) 
         {
             $cart = Cookie::get('cart');
@@ -53,6 +54,7 @@ class CreateCartCookie
                 $cart = Cookie::get('cart');
             } 
         }
+        */
 
         return $next($request);
     }
