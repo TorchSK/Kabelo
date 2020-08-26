f5d85b8513b2d03b83a9016f712d283265c60935
TorchSK <gtorch@gmail.com>
2020-08-26 12:49:37 +0200

    Revert "aa"
    This reverts commit 10f7232f94a5822b1cac498f3a9f2926c55d9a20.

--- resources/views/admin/eshop/new.blade.php
+++ resources/views/admin/eshop/new.blade.php
@@ -13,7 +13,6 @@
 			    <th></th>
 			    <th>Carousel</th>
 			    <th>ID</th>
-			    <th>Kód</th>
 			    <th>Názov</th>
 			    <th>Zmazať</th>
 			  </tr></thead>
@@ -24,7 +23,6 @@
 			 	  <td class="collapsing"><div class="ui checkbox"><input type="checkbox" name="new_carousel" @if($product->new_carousel)checked @endif></div></td>
 
 			      <td>{{$product->id}}</td>
-			   		<td>{{$product->code}}</td>
 			      <td>{{$product->name}}</td>
 			      <td class="collapsing"><a class="ui mini icon red button"><i class="delete large icon"></i></a></td>
 			  	</tr>
