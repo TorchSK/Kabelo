14e8652538d2d52714f92b1edc9ed16b982f1749
TorchSK <gtorch@gmail.com>
2020-10-09 14:49:00 +0200

    aa

--- app/File.php
+++ app/File.php
@@ -24,18 +24,18 @@ class File extends Model {
     {
 		  $array = explode('/', $this->path);
 
-      /*
+      
       if(isset($array[5]) && isset($array[7]))
       {
-    	$path = 'https://dedra.blob.core.windows.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
+    	$path = 'https://cdndedra.azureedge.net/imagehandler/dedra.blob.core.windows.net/cms/ContentItems/'.$array[5].'/images/m_max__w_480__h_480__a_middlecenter__o__x_bottomright__r_30/'.explode('.',$array[7])[0].'.jpeg';
         return $path;
       }
       else{
         return $this->path;
       }
-      */
+      
 
-      return $this->path;
+  
 
     }
 }
